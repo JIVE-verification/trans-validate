e377f99374c5a2dd536e0b4f42d99c6deebd71ea
Jshim-CS <jshim@iastate.edu>
2024-10-01 20:11:30 -0500

    changed source code

--- demo/reduction/reduction.cpp
+++ demo/reduction/reduction.cpp
@@ -1,12 +1,11 @@
 #include<omp.h>
 //#include<stdio.h>
 
-void reduction(int *v2){
+int reduction(){
     int v = 0;
     #pragma omp parallel for reduction(+:v)
     for(int i = 0; i < 10; i++){
         v += i;
     }
-    *v2 = v;
-    //printf("%d\n",v);
+    return v;
 }
